a17dd60fa1f65ad5ae04cae8d3f4f01f1f86a3f5
shgradyy <shantaygrady@gmail.com>
2019-11-23 02:07:02 -0500

    imports useState and useEffect

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import FormContext from '../contexts';
